c5a9fe981288c9750340c161b0adeef8d5373529
gayatri2306 <gayatrigangitla17@gmail.com>
2026-05-30 11:13:18 +0530

    Update server.js

--- ProjectF/backend/server.js
+++ ProjectF/backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://ggayatri_db_user:<db_password>@cluster0.0mkkovw.mongodb.net/");
 
 const User = mongoose.model("User", {
   name: String
